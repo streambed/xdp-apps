31298208278ec030262d7cd4402423c5ec697dec
Christopher Hunt <huntchr@gmail.com>
2019-05-16 15:06:42 +1000

    Re-arrange the chairs
    Specifically, factor out electric fence sensor information in general

---
+++ electric-fence/dashboards/main/source.js
@@ -0,0 +1,43 @@
+// it worked
+
+
+
+function() {
+  return {
+    panels: {
+      latestVoltage: {
+        title: 'Latest Electric Fence Readings',
+        type: 'ObservationCard',
+        data: {
+          topic: 'electric-fence-data-up-json',
+          field: 'voltage'
+        }
+      },
+
+      historicalVoltage: {
+        title: 'Electric Fence Readings',
+        type: 'ObservationTimeSeriesCard',
+        data: {
+          topic: 'electric-fence-data-up-json',
+          field: 'voltage'
+        }
+      }
+
+    },
+
+    layout: [
+      { type: "row", entries: [
+        { type: "col", size: 6, entries: [
+          { type: "row", entries: [
+            { type: "col", size: 6, entries: ["latestVoltage"]}
+          ]}
+        ]},
+        { type: "col", size: 6, entries: [
+          { type: "row", entries: [
+            { type: "col", entries: ["historicalVoltage"]}
+          ]}
+        ]}
+      ]}
+    ]
+  };
+}
