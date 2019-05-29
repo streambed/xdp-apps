4cb650d73a0f063c397bb1516cd3eb955fc846e3
Justin Nhan <justinnhan@hotmail.com>
2019-05-29 11:50:34 +1000

    Fixed transformer and added tests.

--- ict-soil-dendrometer-probe/transformers/main/source.js
+++ ict-soil-dendrometer-probe/transformers/main/source.js
@@ -3,6 +3,12 @@
  *  Transformation code for ICT Dendrometer & Soil Moisture Probe 
  * 
  * ****************************************************************************/
+const STEM_DIAMETER = 0;
+const STEM_TEMPEREATURE = 1;
+const VWC_COUNT = 2;
+const TEMPERATURE = 3;
+const EC = 4;
+const VWC = 5;
 
 /**
  * Extracts bytes from byte_payload from start to end and converts these bytes 
@@ -23,7 +29,7 @@ function extractFloat(byte_payload, start, end) {
   var float = sign * m * Math.pow(2, e - 150);
 
   // Return float in 2 decimal places
-  return Math.round(float*100)/100;
+  return float*100/100;
 }  
 
 /**
@@ -33,38 +39,62 @@ function extractFloat(byte_payload, start, end) {
 function transform (time, nwkAddr, fPort, base64_payload) {
   var payload = atob(base64_payload);
   var commandFlag = payload[9].charCodeAt(0);
-
   var decoded = [];
-  if (commandFlag) {
+  if (!commandFlag) {
     decoded.push (
-      {outlet: 0, data: {time: time, nwkAddr: nwkAddr, 
+      {outlet: STEM_DIAMETER, data: {time: time, nwkAddr: nwkAddr, 
         stemDiameter: extractFloat(payload, 10, 13)}},
-      {outlet: 1, data: {time: time, nwkAddr: nwkAddr, 
+      {outlet: STEM_TEMPEREATURE, data: {time: time, nwkAddr: nwkAddr, 
         stemTemperature: extractFloat(payload, 14, 17)}}
     );
   } else {
+    var vwcCount_decoded = extractFloat(payload, 10, 13);
     decoded.push (
-      {outlet: 2, data: {time: time, nwkAddr: nwkAddr, 
-        vwcCount: extractFloat(payload, 10, 13)}},
-      {outlet: 3, data: {time: time, nwkAddr: nwkAddr, 
+      {outlet: VWC_COUNT, data: {time: time, nwkAddr: nwkAddr, 
+        vwcCount: vwcCount_decoded}},
+      {outlet: TEMPERATURE, data: {time: time, nwkAddr: nwkAddr, 
         temperature: extractFloat(payload, 14, 17)}},
-      {outlet: 4, data: {time: time, nwkAddr: nwkAddr, 
+      {outlet: EC, data: {time: time, nwkAddr: nwkAddr, 
         EC: extractFloat(payload, 18, 21)}},
-      // Access 'decoded' array value for vwcCount stored in index 4 to 
-      // calculate vwc.
-      {outlet: 5, data: {time: time, nwkAddr: nwkAddr, 
-        vwc: ((0.0003879 * decoded[2].data.vwcCount - 0.6956)*100)}})
+      {outlet: VWC, data: {time: time, nwkAddr: nwkAddr, 
+        vwc: ((0.0003879 * vwcCount_decoded - 0.6956)*100)}}
     );
   }
   return decoded;
 }
 
+function assert(condition, message) {
+  if (!condition) {
+    message = message || "Assertion failed";
+    if (typeof Error !== "undefined") {
+      throw new Error(message);
+    }
+    throw message; // Fallback
+  }
+}
+/**
+ * Tests for the two possible payloads depending on command flag.
+ */
 function test() {
-  payload = "AAAHMRBqLy8BAUKKAABD0gAA=="; // Payload for command = true;
-  var result = transform("2019-03-29T06:02:04.539Z", 65959, 15, payload);
-  return result;
+  // Dendrometer payload for command = 0;
+  var payload_0 = "AAAHMRBqLy8BAELMR65CrzMz"; 
+  var result_0 = transform("2019-03-29T06:02:04.539Z", 65959, 15, payload_0);
+  assert (result_0[STEM_DIAMETER].data.stemDiameter === 102.138671875, 
+    'Expected stem diameter = 102.138671875');
+  assert (result_0[STEM_TEMPEREATURE].data.stemTemperature === 87.599609375, 
+    'Expected stem tempreature = 87.599609375');
+    
+  // Soil moisture payload for command = 1;
+  var payload_1 = "AAAHMRBqLy8BAULMR65CrzMzwqxmZg=="; 
+  var result_1 = transform("2019-03-29T06:02:04.539Z", 65959, 15, payload_1);
+  assert (result_1[VWC_COUNT - 2].data.vwcCount === 102.138671875, 
+    'Expected vwcCount = 102.138671875');
+  assert (result_1[TEMPERATURE - 2].data.temperature === 87.599609375, 
+    'Expected temperature = 87.599609375');
+  assert (result_1[EC - 2].data.EC === -86.19921875, 
+    'Expected EC = -86.19921875');
+  assert (result_1[VWC - 2].data.vwc === -65.59804091796875, 
+    'Expected EC = -65.59804091796875');
   
-  // payload = "AAAHMRBqLy8BAEecaoBCigAAQvYAAA=="; // Payload for command = false;
-  // var result = transform("2019-03-29T06:02:04.539Z", 65959, 15, payload);
-  // return result;
+  return 'Tests Passed!'
 }
