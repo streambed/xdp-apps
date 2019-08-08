1e06e8a93808962319a28a2abf6ee09b16b16e6f
Christopher Hunt <huntchr@gmail.com>
2019-08-08 20:19:14 +1000

    New binary format for the electric fence sensor

--- agriace-electric-fence/transformers/main/source.js
+++ agriace-electric-fence/transformers/main/source.js
@@ -1,34 +1,25 @@
 function transform(time, nwkAddr, fPort, payload) {
-  return {
-    time: time,
-    nwkAddr: nwkAddr,
-    voltage: parseFloat(atob(payload))
-  };
+  if (fPort === 1) {
+    var payloadBytes = atob(payload);
+    if (payloadBytes.length >= 1) {
+      return {
+        time: time,
+        nwkAddr: nwkAddr,
+        voltage: parseInt(payloadBytes.charCodeAt(0)) / 10
+      };
+    } else {
+      return [];
+    }
+  } else {
+    return [];
+  }
 }
 
 function test() {
-  const tests = {
-    "voltage of 10": [
-      transform("2019-03-29T06:02:04.539Z", 65959, 15, "MTA="),
-      { time: "2019-03-29T06:02:04.539Z", nwkAddr: 65959, voltage: 10 }
-    ],
-
-    "voltage of 5.5": [
-      transform("2018-03-29T06:02:04.539Z", 62959, 12, "NS41"),
-      { time: "2018-03-29T06:02:04.539Z", nwkAddr: 62959, voltage: 5.5 }
-    ]
-  };
-
-  const testFailures = Object
-    .keys(tests)
-    .map(function(test) {
-      return { name: test, actual: JSON.stringify(tests[test][0]), expected: JSON.stringify(tests[test][1]) };
-    })
-    .filter(function(test) {
-      return test.actual !== test.expected;
-    });
-
-  return testFailures.length === 0 ? true :
-    "[" + testFailures[0].name + "] [" + testFailures[0].actual + "] did not equal expected value [" + testFailures[0].expected + "]";
+  var result = transform("2019-04-12T00:00:00.000Z", 2, 1, "XwA=");
+  return (
+    result.time === "2019-04-12T00:00:00.000Z" &&
+    result.nwkAddr === 2 &&
+    result.voltage === 9.5
+    );
 }
-
