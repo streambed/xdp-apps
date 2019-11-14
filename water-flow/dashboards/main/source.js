b696eaaf074e315515debc0841c894fda5de890b
Christopher Hunt <huntchr@gmail.com>
2019-11-14 18:08:58 +1100

    Show flow as an area
    Also assumes that water flow is cumulative and leverages the fact that observations are passed in reverse order.

--- water-flow/dashboards/main/source.js
+++ water-flow/dashboards/main/source.js
@@ -6,7 +6,30 @@ function() {
           type: 'ObservationTimeSeriesCard',
           data: {
             topic: 'water-flow-data-up-json',
-            field: 'flow15'
+            field: 'flow15',
+            plotType: 'area',
+            aggregate: function(topic, observations) {
+              let maxFlow15 = 0.0;
+              let firstFlow15 = undefined;
+              let prevFlow15 = 0.0;
+              for (let i = observations.length - 1; i >= 0; --i) {
+                const o = observations[i];
+                if (typeof o.flow15 === 'number' && o.flow15 >= 0.0 && o.flow15 < 100000000.0) {
+                  if (firstFlow15 === undefined) {
+                    firstFlow15 = o.flow15;
+                  }
+                  const flow15 = o.flow15 - prevFlow15 - firstFlow15;
+                  if (flow15 > maxFlow15) {
+                    maxFlow15 = flow15;
+                  }
+                  prevFlow15 = flow15;
+                }
+              }
+
+              return {
+                flow15: maxFlow15
+              };
+            }
           }
         }
       },
