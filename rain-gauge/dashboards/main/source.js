0b2fb9459a9dc030c5f452418a618f25a194047e
huntc <huntchr@gmail.com>
2021-06-18 16:34:53 +1000

    Various fixes as a result of deploying to Cara Station

--- rain-gauge/dashboards/main/source.js
+++ rain-gauge/dashboards/main/source.js
@@ -1,25 +1,51 @@
 function() {
   return {
     panels: {
-      latestRain: {
-        title: 'Latest Rain Gauge Readings',
-        type: 'ObservationCard',
+      historicalRain: {
+        title: 'Historical Rain',
+        type: 'ObservationTimeSeriesCard',
         data: {
           topic: 'rain-gauge-data-up-json',
-          field: 'level'
+          field: 'totalLevel',
+          aggregate: function a(topic, observations) {
+            function isValid(level) {
+              return (typeof level === 'number' && level >= 0.0 && level < 100.0);
+            }
+
+            let totalLevel = 0;
+            if (observations.length > 0) {
+              let firstObs = { level: 0 };
+              if (observations.length > 1) {
+                firstObs = observations[0];
+              }
+              const lastObs = observations[observations.length - 1];
+              if (isValid(firstObs.level) && isValid(lastObs.level)) {
+                totalLevel = lastObs.level - firstObs.level;
+              }
+            }
+
+            return {
+              totalLevel: totalLevel
+            };
+          }
         }
       }
-
     },
 
     layout: [
-      { type: "row", entries: [
-        { type: "col", size: 6, entries: [
-          { type: "row", entries: [
-            { type: "col", size: 6, entries: ["latestRain"]}
-          ]}
-        ]}
-      ]}
+      {
+        type: "row", entries: [
+          {
+            type: "col", size: 8, entries: [
+              {
+                type: "row", entries: [
+                  { type: "col", size: 8, entries: ["historicalRain"] }
+                ]
+              }
+            ]
+          }
+        ]
+      }
     ]
   };
 }
--- snip-ws24-ml-weather-station/transformers/main/source.js
+++ snip-ws24-ml-weather-station/transformers/main/source.js
@@ -27,26 +27,32 @@ function transform(time, nwkAddr, fPort, payload) {
 
   if (fPort === 1 && payloadBytes.length >= 2) {
     const header = payloadBytes.charCodeAt(1);
-    if ((header & 0x80) === 0x80) {
+    if ((header & 0x40) === 0x40 && payloadBytes.length >= 18) {
+      return [{
+        outlet: RAIN_GAUGE, data: {
+          "time": time, "nwkAddr": nwkAddr, "level": decodeFloat(payloadBytes, 14) * 0.2
+        }
+      }];
+    } else if ((header & 0x80) === 0x80) {
       const com = header & 0x0f;
-      if (com === 0 && payloadBytes.length >= 6) {
+      if (com === 0 && payloadBytes.length >= 10) {
         return [{
           outlet: WIND_DIRECTION, data: {
             "time": time, "nwkAddr": nwkAddr, "windDirection": decodeFloat(payloadBytes, 2)
           }
         }, {
           outlet: WIND_SPEED, data: {
-            "time": time, "nwkAddr": nwkAddr, "windSpeed": decodeFloat(payloadBytes, 4)
+            "time": time, "nwkAddr": nwkAddr, "windSpeed": decodeFloat(payloadBytes, 6)
           }
         }];
-      } else if (com === 1 && payloadBytes.length >= 6) {
+      } else if (com === 1 && payloadBytes.length >= 10) {
         return [{
           outlet: AIR_TEMPERATURE, data: {
             "time": time, "nwkAddr": nwkAddr, "airTemperature": decodeFloat(payloadBytes, 2)
           }
         }, {
           outlet: RELATIVE_HUMIDITY, data: {
-            "time": time, "nwkAddr": nwkAddr, "relativeHumidity": decodeFloat(payloadBytes, 4)
+            "time": time, "nwkAddr": nwkAddr, "relativeHumidity": decodeFloat(payloadBytes, 6)
           }
         }];
       } else {
@@ -64,11 +70,11 @@ function test() {
   var tests = {
     "wind reading": [
       transform("2020-02-06T23:44:04.539Z", 65959, 1, "E4BDqwAAPczMzQ=="),
-      [{ outlet: WIND_DIRECTION, data: { time: "2020-02-06T23:44:04.539Z", nwkAddr: 65959, windDirection: 342 } }, { outlet: WIND_SPEED, data: { time: "2020-02-06T23:44:04.539Z", nwkAddr: 65959, windSpeed: 2.2168541705618606e-41 } }]
+      [{ outlet: WIND_DIRECTION, data: { time: "2020-02-06T23:44:04.539Z", nwkAddr: 65959, windDirection: 342 } }, { outlet: WIND_SPEED, data: { time: "2020-02-06T23:44:04.539Z", nwkAddr: 65959, windSpeed: 0.10000000149011612 } }]
     ],
     "air reading": [
       transform("2020-02-06T23:44:04.539Z", 65959, 1, "E4FBf4EGQidwpA=="),
-      [{ outlet: AIR_TEMPERATURE, data: { time: "2020-02-06T23:44:04.539Z", nwkAddr: 65959, airTemperature: 15.968999862670898 } }, { outlet: RELATIVE_HUMIDITY, data: { time: "2020-02-06T23:44:04.539Z", nwkAddr: 65959, relativeHumidity: -2.4659374949328326e-38 } }]
+      [{ outlet: AIR_TEMPERATURE, data: { time: "2020-02-06T23:44:04.539Z", nwkAddr: 65959, airTemperature: 15.968999862670898 } }, { outlet: RELATIVE_HUMIDITY, data: { time: "2020-02-06T23:44:04.539Z", nwkAddr: 65959, relativeHumidity: 41.86000061035156 } }]
     ]
   };
 
