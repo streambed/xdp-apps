bb8fd99c9f839d9251b7b956ca4b57bd0413618f
Christopher Hunt <huntchr@gmail.com>
2023-06-14 13:56:37 +1000

    Return temperature also

--- farmo-moisture-temp/transformers/main/source.js
+++ farmo-moisture-temp/transformers/main/source.js
@@ -1,15 +1,24 @@
 function transform(time, nwkAddr, fPort, payload) {
   var payloadBytes = atob(payload);
-  if (payloadBytes.length >= 5) {
+  if (payloadBytes.length >= 7) {
     const vwc = (
       (parseInt(payloadBytes.charCodeAt(4)) << 8) |
       (parseInt(payloadBytes.charCodeAt(5)))
     );
-    return {
+    const temp = (
+      (parseInt(payloadBytes.charCodeAt(6)) << 8) |
+      (parseInt(payloadBytes.charCodeAt(7)))
+    );
+    return [{
       time: time,
       nwkAddr: nwkAddr,
       vwc: vwc * 0.0001
-    };
+    },
+    {
+      time: time,
+      nwkAddr: nwkAddr,
+      temperature: temp * 0.01
+    }];
   } else {
     return [];
   }
@@ -19,7 +28,10 @@ function test() {
   const tests = {
     "voltage": [
       transform("2022-03-02T00:00:00.000Z", 2, 1, "DSMAAAg+CH0AHBA="),
-      { time: "2022-03-02T00:00:00.000Z", nwkAddr: 2, vwc: 0.21100000000000002 }
+      [
+        { time: "2022-03-02T00:00:00.000Z", nwkAddr: 2, vwc: 0.21100000000000002 },
+        { time: "2022-03-02T00:00:00.000Z", nwkAddr: 2, temperature: 21.73 },
+      ]
     ]
   };
 
