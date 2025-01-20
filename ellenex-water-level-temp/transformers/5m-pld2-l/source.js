446c47b229ca611d97b13593bd7d3820a4d3c2b8
huntc <huntchr@gmail.com>
2025-01-20 11:13:15 +1100

    Level should be in mm

--- ellenex-water-level-temp/transformers/5m-pld2-l/source.js
+++ ellenex-water-level-temp/transformers/5m-pld2-l/source.js
@@ -1,20 +1,20 @@
 function transform(time, nwkAddr, fPort, payload) {
   const payloadBytes = atob(payload);
   if (payloadBytes.length >= 7 && parseInt(payloadBytes.charCodeAt(2)) === 0) {
-      const pressure = (
-        (parseInt(payloadBytes.charCodeAt(3)) << 8) |
-        (parseInt(payloadBytes.charCodeAt(4)))
-      );
-      const range = 5000.0; // Cable len
-      const density = 1.0;  // A density of 1 is water
-      const height = pressure / (1000.0 * density);
-      const temp = ((
-        (parseInt(payloadBytes.charCodeAt(5)) << 8) |
-        (parseInt(payloadBytes.charCodeAt(6)))
-      ) / 100.0) - (0.39 * range / 1000.0);
-      return {"time": time, "nwkAddr": nwkAddr, "height": height, "temperature": temp};
+    const pressure = (
+      (parseInt(payloadBytes.charCodeAt(3)) << 8) |
+      (parseInt(payloadBytes.charCodeAt(4)))
+    );
+    const range = 5000.0; // Cable len
+    const density = 1.0;  // A density of 1 is water
+    const height = pressure / density;
+    const temp = ((
+      (parseInt(payloadBytes.charCodeAt(5)) << 8) |
+      (parseInt(payloadBytes.charCodeAt(6)))
+    ) / 100.0) - (0.39 * range / 1000.0);
+    return { "time": time, "nwkAddr": nwkAddr, "height": height, "temperature": temp };
   } else {
-      return [];
+    return [];
   }
 }
 
@@ -22,16 +22,16 @@ function test() {
   const tests = {
     "level and temp": [
       transform("2019-03-29T06:02:04.539Z", 65959, 15, "AD8AFiYJHSQ="),
-      { time: "2019-03-29T06:02:04.539Z", nwkAddr: 65959, height: 5.670, temperature: 21.38 }
+      { time: "2019-03-29T06:02:04.539Z", nwkAddr: 65959, height: 5670, temperature: 21.38 }
     ]
   };
 
   const testFailures = Object
     .keys(tests)
-    .map(function(test) {
+    .map(function (test) {
       return { name: test, actual: JSON.stringify(tests[test][0]), expected: JSON.stringify(tests[test][1]) };
     })
-    .filter(function(test) {
+    .filter(function (test) {
       return test.actual !== test.expected;
     });
 
